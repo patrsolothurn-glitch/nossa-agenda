73ef784a788940bb0a1b4ea7fb08eff6cd57c05a
Patricio <pat@nossaagenda.ch>
2026-08-05 21:30:47 +0000

    build 60 - horas lado a lado, Para quem alinhado à esquerda

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nossa-agenda-v59';
+const CACHE = 'nossa-agenda-v60';
 const ASSETS = ['/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', e => {
