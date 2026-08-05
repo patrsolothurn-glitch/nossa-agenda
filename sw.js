06dc08f06c61f71fb2abc68789a6c01b015b10e6
Patricio <pat@nossaagenda.ch>
2026-08-05 20:49:51 +0000

    build 57 - forceRefresh com URL timestamp, meta no-cache, limpar ?v= do URL

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nossa-agenda-v56';
+const CACHE = 'nossa-agenda-v57';
 const ASSETS = ['/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', e => {
