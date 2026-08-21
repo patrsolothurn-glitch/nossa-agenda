0483a5a969ad381e25edb23c953ac19da1e98508
Patricio <pat@nossaagenda.ch>
2026-08-21 21:46:17 +0000

    build 73 - fix login: else simples sem verificar session

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nossa-agenda-v72';
+const CACHE = 'nossa-agenda-v73';
 const ASSETS = ['/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', e => {
