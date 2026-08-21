cb5bafc9b3b4ac65e8c4278ed81c36f210f9d2b2
Patricio <pat@nossaagenda.ch>
2026-08-21 21:52:20 +0000

    build 74 - FIX CRITICO: erro de sintaxe JS do build 64 quebrava toda a app (aspas partidas em adminArchiveMonth)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nossa-agenda-v73';
+const CACHE = 'nossa-agenda-v74';
 const ASSETS = ['/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', e => {
