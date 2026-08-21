c72540fd2f09dadbbd7f97e4ac1cefa98177c39b
Patricio <pat@nossaagenda.ch>
2026-08-21 21:05:51 +0000

    build 64 - admin: eventos agrupados por mês com botão Arquivar

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nossa-agenda-v63';
+const CACHE = 'nossa-agenda-v64';
 const ASSETS = ['/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', e => {
