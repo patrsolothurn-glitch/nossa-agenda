bf67aa82f0c98f19ae13b77e92f734bf17ea96f2
Patricio <pat@nossaagenda.ch>
2026-08-21 21:19:47 +0000

    build 68 - admin sempre furtivo por defeito, indicador minúsculo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nossa-agenda-v67';
+const CACHE = 'nossa-agenda-v68';
 const ASSETS = ['/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', e => {
