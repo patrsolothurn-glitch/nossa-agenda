0ca5044a274ea08992f69d7cfa1f0be57b5bf583
Patricio <pat@nossaagenda.ch>
2026-08-21 21:18:42 +0000

    build 67 - modo furtivo admin: invisible, sem notificações, indicador discreto

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nossa-agenda-v66';
+const CACHE = 'nossa-agenda-v67';
 const ASSETS = ['/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', e => {
