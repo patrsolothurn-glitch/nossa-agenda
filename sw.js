37404e6d68cdca18ab9b370236a3047b85d0422b
Patricio <pat@nossaagenda.ch>
2026-08-21 21:28:38 +0000

    build 70 - fix CRÍTICO: login não abria a app após autenticação (else boot() desapareceu)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nossa-agenda-v69';
+const CACHE = 'nossa-agenda-v70';
 const ASSETS = ['/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', e => {
