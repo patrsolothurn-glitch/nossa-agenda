9460cacb02e155c0fb1e62db5b9b5681d10ee41e
Patricio <pat@nossaagenda.ch>
2026-08-22 05:33:28 +0000

    build 78 - remover simbolo furtivo, admin ja sabe que esta ativo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nossa-agenda-v77';
+const CACHE = 'nossa-agenda-v78';
 const ASSETS = ['/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', e => {
