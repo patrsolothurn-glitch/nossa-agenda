532bc540537a0b7a09143fe76be0c6469855a5b0
Patricio <pat@nossaagenda.ch>
2026-08-22 05:43:47 +0000

    build 81 - arquivo diario (nao semanal), botao real Adicionar membro abre convite

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nossa-agenda-v80';
+const CACHE = 'nossa-agenda-v81';
 const ASSETS = ['/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', e => {
