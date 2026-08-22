18348c7b0799011d1ca37455fe746b0d2f25b063
Patricio <pat@nossaagenda.ch>
2026-08-22 05:28:39 +0000

    build 77 - admin nunca aparece na barra de membros nem como criador de eventos, para ninguem

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nossa-agenda-v76';
+const CACHE = 'nossa-agenda-v77';
 const ASSETS = ['/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', e => {
